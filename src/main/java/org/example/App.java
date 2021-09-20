dbceb34e892153ecb3d6f97f02029023facbfcd8
hekun1997 <1576053065@qq.com>
2021-09-20 21:01:32 +0800

    leetcode no.1 and no.2

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.math.BigInteger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new BigInteger("100000000000"));
+    }
+}
