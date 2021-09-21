107759e15f245a0f4d4766c9c85f07ea32e32c68
hekun1997 <1576053065@qq.com>
2021-09-21 22:46:41 +0800

    leetcode no.7 and no.13

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        Integer a = 1000000000;
+        System.out.println(a);
+        System.out.println(a * 3);
         System.out.println(new BigInteger("100000000000"));
     }
 }
