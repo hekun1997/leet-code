fb4c7d559c45ad0638679cdb2085e51313c199dc
hekun1997 <1576053065@qq.com>
2022-11-29 19:31:36 +0800

    2022-11-29

--- src/main/java/org/example/_94.java
+++ src/main/java/org/example/_94.java
@@ -5,21 +5,29 @@ import org.example.object.TreeNode;
 import java.util.*;
 
 public class _94 {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        if (Objects.equals(root, null)){
-            return null;
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> retVal = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                retVal.add(curr.val);
+                curr = curr.right;
+            }
         }
-
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.add(root.getLeft());
-        q.add(root);
-        q.add(root.getRight());
-        while (!q.isEmpty()){
-
-        }
-        return null;
+        return retVal;
     }
     public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        a.right = b;
+        b.left = c;
 
+        System.out.println(inorderTraversal(a));
     }
 }
