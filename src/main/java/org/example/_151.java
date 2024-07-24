9f00e639d1269be46c81d8e821065dc0c20cfa22
1 <1576053065@qq.com>
2024-07-24 17:17:06 +0800

    23.3.28 refactor: use isEmpty() instead of equals(" ") in _151.java

--- src/main/java/org/example/_151.java
+++ src/main/java/org/example/_151.java
@@ -48,11 +48,9 @@ public class _151 {
         s = s.trim();
         StringBuilder ret = new StringBuilder();
 
-        int index = 0;
-
         String[] arr = s.split(" ");
         for (String temp : arr) {
-            if (!temp.equals(" ") && !temp.equals("")) {
+            if (!temp.equals(" ") && !temp.isEmpty()) {
                 ret.append(temp).append(" ");
             }
         }
@@ -61,22 +59,6 @@ public class _151 {
     }
 
     public static void main(String[] args) {
-        /**
-         * 示例 1：
-         *
-         * 输入：s = "the sky is blue"
-         * 输出："blue is sky the"
-         * 示例 2：
-         *
-         * 输入：s = "  hello world  "
-         * 输出："world hello"
-         * 解释：反转后的字符串中不能存在前导空格和尾随空格。
-         * 示例 3：
-         *
-         * 输入：s = "a good   example"
-         * 输出："example good a"
-         * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
-         */
         String s = "EPY2giL";  //EPY2giL
         String out = "example good a";
         System.out.println(reverseWords(s));
